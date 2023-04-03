d6e6c209191d966f8f23d63d72db79a0dce9e4d7
JFlorentino45 <jarryd.florentino@gmail.com>
2023-04-03 16:28:46 +0200

    Added edit venues

---
+++ pages/editVenue.php
@@ -0,0 +1,48 @@
+<?php 
+include('./connection/connectionString.php');
+$venueID = $_GET['id'];
+
+$stmt = $conn->prepare("SELECT * FROM venue WHERE venueID = :id");
+$stmt->execute(['id' => $venueID]);
+$venue = $stmt->fetch();
+
+if(isset($_POST['submit'])) {
+    // Get the event details from the form
+    $venueName = $_POST['venueName'];
+    $venueAddress = $_POST['venueAddress'];
+    $venuePostalCode = $_POST['venuePostalCode'];
+    $venueImg = $_POST['venueImg'];
+
+    $stmt = $conn->prepare("UPDATE venue SET venueName = :venueName, venueAddress = :venueAddress, venuePostalCode = :venuePostalCode, venueImg = :venueImg WHERE venueID = :venueID");
+    $stmt->bindParam(':venueName', $venueName);
+    $stmt->bindParam(':venueAddress', $venueAddress);
+    $stmt->bindParam(':venuePostalCode', $venuePostalCode);
+    $stmt->bindParam(':venueImg', $venueImg);
+    $stmt->bindParam(':venueID', $venueID);
+    $stmt->execute();
+
+    header('Location: index.php?page=venues');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Venue</title>
+</head>
+<body>
+    <h1>Edit Venue</h1>
+    <form method="post">
+        <label>Venue Name:</label>
+        <input type="text" name="venueName" value="<?php echo $venue['venueName']; ?>">
+        <label>Venue Address:</label>
+        <input type="text" name="venueAddress" value="<?php echo $venue['venueAddress']; ?>">
+        <label>Venue Postal Code:</label>
+        <input type="text" name="venuePostalCode" value="<?php echo $venue['venuePostalCode']; ?>">
+        <label>Venue Image URL:</label>
+        <input type="text" name="venueImg" value="<?php echo $venue['venueImg']; ?>">
+        <input type="submit" name="submit" value="Edit Venue">
+    </form>
+    </body>
+</html>
--- pages/venues.php
+++ pages/venues.php
@@ -11,6 +11,7 @@ foreach ($venues as $venue) {
     <h1><?php echo $venue['venueName']; ?></h1>
     <img class="venueImg" src="<?php echo $venue['venueImg']; ?>" alt="<?php echo $venue['venueName']; ?>">
     <p>Address: <?php echo $venue['venueAddress']. " ". $venue['venuePostalCode']; ?></p>
+    <a href="index.php?page=editVenue&id=<?php echo $venue['venueID'] ?>"><button>Edit</button></a>
     <?php
 }
 ?>
